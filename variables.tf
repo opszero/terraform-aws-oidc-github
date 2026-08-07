3d9620b01713dfba1967654f5404db2aaa7783bc
Abhi Yerra <abhi@opszero.com>
2026-08-07 16:46:31 -0700

    Remove validation from github_repositories variable
    Removed validation for github_repositories variable to simplify configuration.

--- variables.tf
+++ variables.tf
@@ -68,16 +68,6 @@ variable "force_detach_policies" {
 variable "github_repositories" {
   description = "List of GitHub organization/repository names authorized to assume the role."
   type        = list(string)
-
-  validation {
-    // Ensures each element of github_repositories list matches the
-    // organization/repository format used by GitHub.
-    condition = length([
-      for repo in var.github_repositories : 1
-      if length(regexall("^[A-Za-z0-9_.-]+?/([A-Za-z0-9_.:/-]+[*]?|\\*)$", repo)) > 0
-    ]) == length(var.github_repositories)
-    error_message = "Repositories must be specified in the organization/repository format."
-  }
 }
 
 variable "iam_role_name" {
